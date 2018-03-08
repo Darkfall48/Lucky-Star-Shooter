bd16e295e7e5b38f526f06c38f3c128c340c62f6
Darkfall48 <sidney@sebban.be>
2018-03-09 00:20:18 +0200

    Added 2 test of individual components for the project that are working in individual.

---
+++ Project Code/Working/Individual/MicroServo/MicroServo_Work/MicroServo_Work.ino
@@ -0,0 +1,32 @@
+// SERVO pin definitions
+
+#include <Servo.h>
+
+Servo Servo_1; 
+
+const int servoAwake = 90; // Awake
+const int servoSleep = 0; // Sleep
+
+// Game Variables
+
+int randNumber;
+
+void setup() 
+{
+  
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+
+  Servo_1.attach(9);
+
+  
+}
+
+void loop() 
+{
+  Servo_1.write(servoSleep);
+  delay(500);
+  Servo_1.write(servoAwake);
+  delay(500);
+
+}
---
+++ Project Code/Working/Individual/PhotoResistor/PhotoResistor_Work/PhotoResistor_Work.ino
@@ -0,0 +1,22 @@
+// Sensor pin definitions
+
+const int Sensor_1 = A0;
+int Anas_1;
+
+void setup() 
+{
+  Serial.begin(9600);  //Begin serial communcation
+  
+
+}
+
+void loop() 
+{
+  //Reading the Sensors values
+  Anas_1 = analogRead(Sensor_1);
+  
+  //Writing the Sensors values
+  Serial.print("Sensor 1 = ");
+  Serial.println(Anas_1);
+  delay(200);
+}
