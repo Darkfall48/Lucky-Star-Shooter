7e83a30d32aec5a3d6acb08ce07601e492a5742c
cywiakh <cywiakh2@gmail.com>
2018-03-09 21:17:40 +0200

    Basic Game Flow Model

--- Project Code/Working/Complete Work/One_Shot/One_Shot.ino
+++ Project Code/Working/Complete Work/One_Shot/One_Shot.ino
@@ -4,28 +4,66 @@
 
 Servo Servo_1; 
 
-const int servoAwake = 0; // Awake
-const int servoSleep = 90; // Sleep
+const int servoAwake           = 0; // Awake
+const int servoSleep           = 90; // Sleep
+
+// Game States
+const int GAME_STATE_RESET     = 0;
+const int GAME_STATE_RUN       = 1;
+const int GAME_STATE_GAME_OVER = 2;
+const int GAME_STATE_WIN       = 3;
+const int GAME_STATE_TARGET    = 4;
 
 // Sensor pin definitions
-
 const int Sensor_1 = A0;
+
 int Anas_1;
 
 // Game Variables
-
 int randNumber;
+int currentGameState;
+int gameScore = 0;
 
-void setup() 
-{
-  Serial.begin(9600);  //Begin serial communcation
+void setup() {
+  Serial.begin(9600);  //Begin serial communication
   randomSeed(analogRead(0));
+  currentGameState = GAME_STATE_RESET;
   
   Servo_1.attach(9);
 }
 
-void loop() 
-{
+void loop() {
+  switch(currentGameState) {
+    // Set targets to down position and initialize game variables
+    case GAME_STATE_RESET:
+      gameScore = 0;
+      currentGameState = GAME_STATE_TARGET;
+      break;
+    case GAME_STATE_TARGET:
+      // Pick a target
+      
+      // Get reference target variables: sensor and servo
+      
+      currentGameState = GAME_STATE_RUN;
+      break;
+    case GAME_STATE_RUN:
+      // Check reset button state, if on then change currentGameState to GAME_STATE_RESET
+      
+      // check sensor value: if hot then add 1 to score and change game state to GAME_STATE_TARGET
+      
+      // Keep track of time: if too long then change game state to GAME_STATE_GAME_OVER
+      
+      // Keep track of score: if higher than 30 then change game state to GAME_STATE_WIN
+      
+      break;
+    case GAME_STATE_GAME_OVER;
+      // Play loser sound
+      break;
+    case GAME_STATE_WIN:
+      // Play winner sound
+      break;
+  }
+  
   //Initialize the Servo
   Servo_1.write(servoSleep);
   delay(500);
@@ -38,15 +76,15 @@ void loop()
   Serial.println(Anas_1);
   delay(200);  
 
-  if (Anas_1 > 90)
-    {
+  if (Anas_1 > 90) {
       Servo_1.write(servoAwake);
       delay(500);
-    }
+  }
     
-  else
-  {
+  else {
     Serial.print("waiting for laser");
   }
 }
+
+
 
